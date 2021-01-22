ea16c48622e27836169ea68e4ecc8f69aae3f5bd
Nathan Davis <nathand120@gmail.com>
2021-01-21 20:59:22 -0700

    Wrote removeFromBlock for board. All tests passing

--- sudoku/src/sudoku/board.clj
+++ sudoku/src/sudoku/board.clj
@@ -10,6 +10,7 @@
 ;; The inner arrays contain cells within a row
 ;; The cells contain an array of possible values
 
+
 ;; This function takes:
 ;; 1. Vector with items
 ;; 2. A function that signifies then to change the item (true) and when to leave it (false)
@@ -23,6 +24,19 @@
             items)))
 
 
+;; This function takes:
+;; 1. Board (vector of vectors)
+;; 2. fnIndexFilter - Function takes row-index, col-index => change the item (true), leave it (false)
+;; 3. fnItemApply - Function that the item and returns a similar item
+(defn walkBoard [items fnIndexFilter fnItemApply]
+  (into [] (for [row-index (range (count items))]
+    (into [] (for [col-index (range (count (get items row-index)))]
+               (let [item (get (get items row-index) col-index)]
+                 (if (fnIndexFilter row-index col-index)
+                   (fnItemApply item)
+                   item)))))))
+
+
 ;; Remove a value from the cell
 ;; A cell is simply a vector of integers
 ;; Return a new vector with all the values except val
@@ -31,6 +45,10 @@
 
 
 ;; Remove the value at the coordinate from the other cells on the row
+;; board - the entire board structure
+;; m - the row where the cell is located
+;; n - the column where the cell is located
+;; * It's assumed that the cell at (m, n) has at least one value in it
 (defn removeFromRow [board, m, n]
   (let [val-to-remove (first (get (get board m) n))]
     (mapvFilterByIndex board
@@ -39,31 +57,36 @@
                                                     (fn [col-index] (not= n col-index)) ;; apply to every cell in the row except cell n
                                                     (fn [cell] (removeFromCell cell val-to-remove)))))))
 
-    ; ;; Loop through the rows
-    ; (into [] (map-indexed
-    ;           (fn [row-index row]
-    ;             (if (not= row-index m)
-    ;               row
-    ;             ;; Loop through the columns
-    ;               (map-indexed
-    ;                (fn [cell-index cell]
-    ;                  (if (= cell-index n)
-    ;                    cell
-    ;                    (removeFromCell cell val-to-remove)))
-    ;                row)))
-    ;           board)))
-    ; )
+
+;; Remove the value at the coordinate from the other cells on the column
+;; 
+(defn removeFromCol [board, m, n]
+  (let [val-to-remove (first (get (get board m) n))]
+    (mapvFilterByIndex board
+                       (fn [row-index] (not= m row-index)) ;; apply to every row except the one with the cell
+                       (fn [row] (mapvFilterByIndex row
+                                                    (fn [col-index] (= n col-index)) ;; apply only to the column with the cell in it
+                                                    (fn [cell] (removeFromCell cell val-to-remove)))))))
 
 
-  ; (let [row (get board m)
-  ;       val-to-remove (first (get row n)) ; define the value to remove
-  ;       remove-val-from-cells (fn [cell] (remove #(= val-to-remove %) cell)) ; define a function that matches that value 
-  ;       new-row (mapv remove-val-from-cells row)]
-  ;   ; NEXT: Need to add that item back to the original cell
-  ;   ; OR: Skip removing it from that cell
-  ;   (assoc board m new-row)))
+;; isSameBlock
+;;
+(defn isSameBlock [m1 n1 m2 n2 block-width block-height]
+  (and (= (quot m1 block-height) (quot m2 block-height)) 
+       (= (quot n1 block-width) (quot n2 block-width))))
+
+;; Remove the value at the coordinate from the other cells in the 
+(defn removeFromBlock [board, m, n, block-width]
+  (let [val-to-remove (first (get (get board m) n))
+        block-height (/ (count board) block-width)]
+    (walkBoard board
+               (fn [row-index col-index] (and (isSameBlock m n row-index col-index block-width block-height) 
+                                              (not (and (= row-index m)(= col-index n))))) 
+               (fn [cell] (removeFromCell cell val-to-remove)))))
 
 (def l (list (list (list 5))))
+
+(quot 1 2)
 
 (remove #(= 3 %) (list 1 2 3 4 5))
 
@@ -79,4 +102,6 @@
 (filterv (fn [item] (not= 3 item)) [1 2 3])
 
 (into [] (map-indexed (fn [index item] [:li index item]) ["rattata" "pidgey" "spearow"]))
+
+(into [] (for [x (range 6)] x))
 
--- sudoku/test/sudoku/board_test.clj
+++ sudoku/test/sudoku/board_test.clj
@@ -3,17 +3,6 @@
             [sudoku.core :refer :all]
             [sudoku.board :refer :all]))
 
-(deftest board-removeFromRow
-  (testing "The value from the coordinate should be removed from that row"
-    (let
-     [initial-board [[[1 2] [2 3] [1]]
-                     [[1] [2 3] [1 2]]]
-      expected-board [[[2] [2 3] [1]]
-                      [[1] [2 3] [1 2]]]]
-      (is (=
-           (removeFromRow initial-board 0 2)
-           expected-board)))))
-
 (deftest board-mapvFilterByIndex
   (testing "mapvFilterByIndex should take a vector, fnFilterItem, fn. It should only apply"
     (let
@@ -25,3 +14,94 @@
       (is (=
            (mapvFilterByIndex initial-vector index-filter item-map)
            expected-vector)))))
+
+(deftest board-walkBoard
+  (testing "walkBoard should take a vector of vectors, filterFn and applyFn. It should only applyFn when filterFn comes back true"
+    (let
+     [initial-board [[1 2 3 4 5]
+                     [6 7 8 9 10]]
+      index-filter (fn [row-index col-index] (= 2 (+ row-index col-index)))
+      apply-fn (fn [value] (+ value 10))
+      expected-board [[1 2 13 4 5]
+                     [6 17 8 9 10]]]
+      (is (= expected-board (walkBoard initial-board index-filter apply-fn))))))
+
+(deftest board-removeFromRow
+  (testing "The value from the coordinate should be removed from that row"
+    (let
+     [initial-board [[[1 2] [2 3] [1]]
+                     [[1] [2 3] [1 2]]]
+      expected-board [[[2] [2 3] [1]]
+                      [[1] [2 3] [1 2]]]]
+      (is (=
+           (removeFromRow initial-board 0 2)
+           expected-board)))))
+
+(deftest board-removeFromCol
+  (testing "The value from the coordinate should be removed from that column"
+    (let
+     [initial-board [[[1 2] [2 3] [1]]
+                     [[1] [2 3] [1 2]]]
+      expected-board [[[1 2] [2 3] [1]]
+                      [[1] [2 3] [2]]]]
+      (is (=
+           (removeFromCol initial-board 0 2)
+           expected-board)))))
+
+(deftest board-removeFromBlock
+  (testing "The value from the coordinate should be removed from that block (1x2 block)"
+    (let
+     [initial-board [[[1] [1 2]]
+                     [[1 2] [1]]]
+      expected-board [[[1] [1 2]]
+                     [[2] [1]]]]
+      (is (=
+           (removeFromBlock initial-board 0 0 1) ;; block-width = 1 ; blocks are 1x2 
+           expected-board))))
+  (testing "The value from the coordinate should be removed from that block (2x1 block)"
+    (let
+     [initial-board [[[1] [1 2]]
+                     [[1 2] [1]]]
+      expected-board [[[1] [2]]
+                     [[1 2] [1]]]]
+      (is (=
+           (removeFromBlock initial-board 0 0 2) ;; block-width = 2 ; blocks are 2x1
+           expected-board)))))
+
+(deftest board-isSameBlock
+  (testing "Given two sets of coordinates, determine if they are in the same block"
+
+    ;; block-width = 3
+    ;; block-height = 2
+    ;; first coord (1, 5)
+    ;; second coord (1, 6)
+    ;;
+    ;; x x x  x x x  x x x 
+    ;; x x x  x x o  o x x 
+    ;;
+    ;; x x x  x x x  x x x 
+    ;; x x x  x x x  x x x 
+    ;;
+    ;; x x x  x x x  x x x 
+    ;; x x x  x x x  x x x 
+    (is (false? (isSameBlock 1 5 1 6 3 2)))
+    (is (false? (isSameBlock 1 5 2 5 3 2)))
+
+    ;; block-width = 3
+    ;; block-height = 2
+    ;; first coord (1, 5)
+    ;; second coord (0, 5)
+    ;;
+    ;; x x x  x x o  x x x 
+    ;; x x x  x x o  x x x 
+    ;;
+    ;; x x x  x x x  x x x 
+    ;; x x x  x x x  x x x 
+    ;;
+    ;; x x x  x x x  x x x 
+    ;; x x x  x x x  x x x 
+    (is (true? (isSameBlock 1 5 0 5 3 2)))
+    (is (true? (isSameBlock 1 5 0 3 3 2)))
+
+    ;; The same two coords should always be in the same block
+    (is (true? (isSameBlock 2 5 2 5 2 3)))))
