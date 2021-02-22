ead4cfbace73c33aa59e5b4a1198063c626ba165
Nathan Davis <u0777729@utah.edu>
2021-02-22 09:01:47 -0700

    Attempting to make a runnable

---
+++ santorini/runable.sh
@@ -0,0 +1,5 @@
+# Taken from https://stackoverflow.com/a/65520268/1457295
+
+#!/bin/bash
+exec java -jar /home/u0777729/cs6963-functional-programming/santorini/target/uberjar/santorini-0.1.0-SNAPSHOT-standalone.jar
+ 
--- santorini/src/santorini/core.clj
+++ santorini/src/santorini/core.clj
@@ -2,15 +2,23 @@
   (:require
    [santorini.turn :as turn]
    [santorini.game :as game]
-   [santorini.strategy :as strategy])
+   [santorini.strategy :as strategy]
+   [clojure.java.io :as cjio])
   (:gen-class))
+
+(defn stdout
+  "Send a string to stdout"
+  [s]
+  (cjio/copy s *out*)
+  (cjio/copy "\n" *out*)
+  (.flush *out*))
 
 (defn outputGame
   "Prepare a game for output"
   [g]
   (if (game/inSetup? g)
-    (game/toJSON g)
-    (game/toJSON (game/incTurn g))))
+    (stdout (game/toJSON g))
+    (stdout (game/toJSON (game/incTurn g)))))
 
 (defn -main
   "Take a game board and return the next move"
@@ -18,7 +26,7 @@
   (loop []
     (let [g (game/fromJSON (read-line))]
       (if (game/inSetup? g)
-        (println (outputGame (strategy/startingPositions g)))
-        (println (outputGame (strategy/pickGame (turn/turnOutcomes g))))))
+        (outputGame (strategy/startingPositions g))
+        (outputGame (strategy/pickGame (turn/turnOutcomes g)))))
     (recur))
   )
