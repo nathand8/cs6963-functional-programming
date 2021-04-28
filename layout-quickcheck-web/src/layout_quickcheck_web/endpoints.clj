b63a0625e0df77899a858f2b71d001b15f9678a8
Nathan Davis <u0777729@utah.edu>
2021-04-27 21:23:43 -0600

    Endpoint working - returning all the bugs as json string

--- layout-quickcheck-web/src/layout_quickcheck_web/endpoints.clj
+++ layout-quickcheck-web/src/layout_quickcheck_web/endpoints.clj
@@ -1,6 +1,20 @@
-(ns layout-quickcheck-web.endpoints)
+(ns layout-quickcheck-web.endpoints
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def bug-folder (io/file "../layout-quickcheck-web"))
+
+;; Open a file
+;; (first (map #(.getPath %)
+;;         (filter #(clojure.string/ends-with? % "data.json")
+;;                 (file-seq bug-folder))))
 
 (defn get-all-bugs [req]
-  {:status 200
-   :headers {"Content-Type" "application/json"}}
-  "You got all the bugs!")
+  (let [bug-folder (io/file "/home/u0777729/cs6963-functional-programming/layout-quickcheck-bugs")
+        all-bugs-json (json/write-str (map #(json/read-str (slurp %) :key-fn keyword)
+                                           (filter #(string/ends-with? % "data.json")
+                                                   (file-seq bug-folder))))]
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body all-bugs-json}))
